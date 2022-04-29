acfe9f3531c626997a63ae6c8c42f4e0968e3da3
Sylva Elendu <iamsylvaelendu@gmail.com>
2022-04-29 21:05:52 +0300

    linted

--- app/core/AppConstants.js
+++ app/core/AppConstants.js
@@ -94,7 +94,7 @@ export default {
     MEDIUM: 'medium',
     HIGH: 'high',
     MARKET: 'market',
-		AGGRESSIVE: 'aggressive',
+    AGGRESSIVE: 'aggressive',
   },
   GAS_TIMES: {
     UNKNOWN: 'unknown',
